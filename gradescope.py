4e00b08d767d17ebba0e75126b482cfdb2e52183
Dave Boku <dsboku26@colby.edu>
2023-10-16 12:53:35 -0400

    Link to repo that hosted code to convert pngToText and perform the problem to page matching. Tons of dependencies needed for Google Cloud Vision API. All files and directories added to this repo.

--- gradescope.py
+++ gradescope.py
@@ -2,77 +2,82 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from flask import Flask, jsonify, request
+from flask_cors import cross_origin
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import html
 import re
 
+
 #Use email and password data from popup.js to login to Gradescope
 app = Flask(__name__)
 
+def image_parser(main_content):
+    #Sort and parse for image links
+    background_images = main_content.find_all(style=re.compile(r'background-image:\s*url\([^)]+\)'))
+    image_pattern = r'https://[^"]+'
+    result_images = []
+    #Parse images
+    for image in background_images:
+        url = re.findall(image_pattern, image['style'])
+        url = [html.unescape(u) for u in url]
+        result_images.extend(url)
+    return result_images
+
+# Function to parse problem numbers
+def problem_number_parser(soup, max_problem_length=6):
+    result_problems = []
+    problem_outline = soup.find_all('div', class_='selectPagesQuestionOutline--title')
+    problem_pattern = r'</strong>(.*?)</div>'
+    for problem in problem_outline:
+        num = re.search(problem_pattern, str(problem))
+        num_parsed = num.group(1).strip()
+        if len(num_parsed) <= max_problem_length:
+            result_problems.append(num_parsed)
+    return result_problems
+
+
 @app.route('/get-data', methods=['POST'])
+@cross_origin(origin='localhost', headers=['Content-Type', 'Authorization'])
 def get_data():
-    global received_email, received_password
     received_data = request.json
-    received_email = received_data.get('email')
-    received_password = received_data.get('password')
-    return jsonify({"email": received_email, "password": received_password})
+    email = received_data.get('email')
+    password = received_data.get('password')
+
+    print(email)
+    print(password)
+
+    # Setup selenium driver
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
+
+    # Automate sign-in process
+    driver.get("https://www.gradescope.com/login")
+    driver.find_element_by_id("session_email").send_keys(email)
+    driver.find_element_by_id("session_password").send_keys(password)
+    driver.find_element_by_name("commit").click()
+
+    # Wait until the page loads
+    WebDriverWait(driver, timeout=10).until(lambda x: x.execute_script("return document.readyState === 'complete'"))
+
+    # Access and parse content
+    driver.get("https://www.gradescope.com/courses/567436/assignments/3290069/submissions/193818048/select_pages")
+    html_content = driver.page_source
+    driver.quit()
+    soup = BeautifulSoup(html_content, "html.parser")
+    main_content = soup.find("main")
+
+    # Use the parsing functions
+    image_links = image_parser(main_content)
+    problem_numbers = problem_number_parser(soup)
+
+    # Print the results
+    print(image_links)
+    print(problem_numbers)
+
+    return jsonify({"status": "success", "image_links": image_links, "problem_numbers": problem_numbers})
 
 if __name__ == '__main__':
     app.run(port=5000)
 
-email, password = get_data()
-
-# Setup selenium driver
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-# chrome_options.add_extension()
-driver = webdriver.Chrome(options=chrome_options)
-
-# Navigate to gradescope.com
-driver.get("https://www.gradescope.com/login")
-
-# Automate sign-in process
-driver.find_element("id", "session_email").send_keys(email)
-driver.find_element("id", "session_password").send_keys(password)
-driver.find_element("name", "commit").click()
-
-# Wait until page loads
-WebDriverWait(driver, timeout=10).until(lambda x: x.execute_script("return document.readyState === 'complete'"))
-
-# Access problem-selection page
-# URL NEEDS TO BE CHANGED DEPENDING ON WHAT USER WANTS TO ACCESS!
-driver.get("https://www.gradescope.com/courses/567436/assignments/3290069/submissions/193818048/select_pages")
-
-# Save content and parse
-html_content = driver.page_source
-driver.quit()
-soup = BeautifulSoup(html_content, "html.parser")
-main_content = soup.find("main")
-
-# Sort and parse for image links
-background_images = main_content.find_all(style=re.compile((r'background-image:\s*url\([^)]+\)')))
-image_pattern = r'https://[^"]+'
-
-# Parse image links
-result_images = []
-for image in background_images:
-    url = re.findall(image_pattern, image['style'])
-    html.unescape(url)
-    result_images.extend(url)
-
-# Parse problem numbers
-result_problems = []
-problem_outline = soup.find_all('div', class_='selectPagesQuestionOutline--title')
-problem_pattern = r'</strong>(.*?)</div>'
-
-# Finalize problem parsing
-for problem in problem_outline:
-    num = re.search(problem_pattern, str(problem))
-    num_parsed = num.group(1).strip()
-    if len(num_parsed) <= 3: # Depends how many problems
-        result_problems.append(num_parsed)
-
-
-print(result_images)
-print(result_problems)
---
+++ pngToText.py
@@ -0,0 +1,157 @@
+import os, io
+import requests
+from io import BytesIO
+# from google.cloud import vision
+from google.cloud import vision_v1
+from pdf2image import convert_from_path
+from google.cloud import vision
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ServiceAccountToken.json'
+
+# WILL TAKE INPUT FROM GRADESCOPE.PY
+test_pdf_path = 'resources/testPDF.pdf'
+image_url = 'https://production-gradescope-uploads.s3-us-west-2.amazonaws.com/uploads/page/file/855388146/page_3.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIAV45MPIOW2IZ34NWW%2F20230917%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230917T062613Z&X-Amz-Expires=10800&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEJ7%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLXdlc3QtMiJHMEUCIFnUiXsqXt2fUguI3BmEfEwdBZkt7b%2F%2BW1FN7FXyjH3qAiEA0jpZ83D9818K%2FJM0ClI7rR%2FRf8AKpH31cQALi2j2Sr4qxAUIh%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARAAGgw0MDU2OTkyNDkwNjkiDIHs35YuN7GDVZLRGiqYBa1uM0UlcbiqAptKnIDvI8qV8BONNnvRms1gKDYg2MJo1PmPc7sPgZ9UsmKFJmDLshfF5nKmZz1AsJtr1irtlyxqCJyAOmpTnYSmykkA%2BsfEzgcv5qaBdmsDdlK8lGKcEE%2BUH69XY2cW%2FRTm7sMFUul8uRNCRaFX%2FZ0uIIYz6nhBfBBHM1gCX4IFwxzIHDwQj45Vzqt6B1lcjA80t2qvKNyaGGFO62DdTdOp36hHLsKWS%2B3bzKMDjAHzIFXRThUVFlyOSL8vA6RQydik5m8SGoZST7YaNVnoQ7c8IGpX118ZE2EUJQ6PCsPCNpmdk9QjwD4EKNjIX%2BpnYuRueZPU63IGo3iUlVKF4a4MAG9KZU9L7Bgjs4UyEB%2BajY2rV6GUdEiNkF21IfQBEezBz98ki569h6Xb7L0Odef8cRJrqvqHTvmYRUoW9HWWxNOjBkTdD%2BUSadN%2FEXyvG%2BsFR5NY1YYi%2BTHeI0%2BrN0cFd98i1%2Bo01DynSM2rVkk5C4MHpBYYZ7smdZTNw4jkx1hsK9PNtt7WL7nt%2FXsFkU0pAJrgSZ3vHcniFim3YjhITJuOaOsOPMbmu9dURRsmh016rw2a%2FYwc1avqcpVihwlzrMNiHXEQ9ffmZLFYyLyy%2FCSuk7%2Bmo1%2FuD6UhwjISDXMbbdxww69inzIxersSNxMYk4ekeRHS%2Fyu%2B6Ph0myNT%2F0jPIvuH9X3%2B88zXfAwAfyuBX13uvjZTmMXPUGKk8hE83VnMGRCfe%2B9MP2MpxWPdwRYtnSdfW9MKDwbWL%2BytDQO9oTmj8nKp%2F0AP8pKGg7FFmCXHSBH8UTpiFxzHA%2F%2FRSWn65Z9XT7lP9LGwNmmlgIhEq4y611sTAM%2F2f3nh7g89qLIydMThjmHfFZhwIpYwzq6aqAY6sQGwDCfxj2%2FsC2DbbmIJTtWrP0NDIEt3lMQoyTNonI4MsjpT24YZAgPZeDQt8BHDjlLCwPar4KXeom9pPE2bCqC6Q2DaSAWU9tWwVzbt%2FBZJBMwJtyfOdX1aa2t3cGd7zQPQ%2BoQbfh6IZ5tyER1e8jZi3nttBoookndo8dDvTsEMkoP37y%2FF5SsaTrDAGi0qmjS6vdwn4ScuJulzRC978oOH8Iaf4Y%2Ffs9KTnROUI0ljR9o%3D&X-Amz-SignedHeaders=host&X-Amz-Signature=5c6202c8e541980afed5bbf3c79a10c2a917633f29f2a4987dbbcda52a24d476'
+arr_of_img_urls = ['resources/page0.png', 'resources/page1.png', 'resources/page2.png']
+example_list_of_problems = ['1.', '2.', '3a.', '3b.', '3c.', '3d.', '3e.', '3f.', '3g.', '4.', '5.']
+
+# test list of problems and page contents for testing problem_and_pages:
+test1 = ["1a. blah blah balh\nblah blah bklah\nblah blah\n1b. hello everyone!!!!!\n", "1c. blah blah balh\nblah blah bklah\nblah blah\n2. hello ebveryone!!!!!\n", "2nd question continued over here\n3. Debugging this is nutssss.\n help me\n"]
+test2 = ['1a.', '1b.', '1c.', '2.', '3.']
+
+# converts the given image URL to text
+# the output's page numbers correspond to the index in the array + 1
+def image_url_to_text(image_urls):
+
+    strings = []
+
+    client = vision_v1.ImageAnnotatorClient.from_service_account_file('ServiceAccountToken.json')
+    
+    for url in image_urls:
+        response = requests.get(url)
+        image_data = response.content
+        url = vision_v1.Image(content=image_data)
+        response = client.text_detection(image=url)
+        texts = response.text_annotations
+        strings.append(texts[0].description)
+    
+    # Uncomment the 2 lines underneath to test this function
+    # for string in strings:
+    #     print(string + "\n\n\n\n\n\n\n")
+
+    return strings
+# print(f'image_url_to_text: {image_url_to_text(arr_of_img_urls)}')
+
+# returns a string version of the given image (local path of image)
+def detect_text(path):
+
+    client = vision.ImageAnnotatorClient()
+
+    with open(path, "rb") as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+
+    if response.error.message:
+        raise Exception(
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(response.error.message)
+        )
+
+    return texts[0].description
+
+# given a list of images from relative path, return list of strings of image contents
+def detect_text_in_imgs(list_of_images):
+    
+    arr = []
+
+    for image in list_of_images:
+        arr.append(detect_text(image))
+
+    return arr
+
+# takes a pdf, returns an array of strings, each element is 1 page of the pdf's text
+def pdf_to_array_of_pages(pdf_path):
+
+    # store pdf
+    images = convert_from_path(pdf_path)
+
+    # number of pages in the given pdf
+    number_pages = len(images)
+
+    # array of strings; each element corresponds to 1 page
+    page_contents = []
+
+    # save each page as an image in directory
+    for i in range(number_pages):
+        curr = 'page' + str(i) + '.png'
+        images[i].save(curr, 'PNG')
+        page_contents.append(detect_text(curr))
+        os.remove(curr)
+    
+    return page_contents
+
+# pdf_to_array_of_pages('resources/example.pdf')
+
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# takes in a list of strings (image texts) and another list of strings (problem numbers) and returns a hashmap 
+# with the key being a problem number and the value being an array of integers corresponding to the problem number's
+# associated pages
+# IN ORDER FOR BEST RESULTS, PROBLEM NUMBER WILL ALWAYS FOLLOW THE FOLLOWING FORMAT:
+# - BEGINNING OF LINE + NUMBER + PART OF PROBLEM + '.'
+# - example: '1a.' or '2c.' or '42e.' or '1.' or '5.'
+# - Since we are getting the problem numbers from the website, please follow the exact format used in gradescope.
+# - It is assumed that each problem number will always be on the left-most side of the line, to allow for easy
+#   parsing of textual information
+# - All numbering should be in the same vertical column across all pages of the assignment 
+
+def page_assigner(list_of_strings, problem_queue):
+    page_num = 0
+    curr_problem = problem_queue.pop(0)
+    next_problem = problem_queue.pop(0)
+    problem_pages_map = {}
+    
+    # iterate through each page
+    for i in range(len(list_of_strings)):
+        text_on_page = list_of_strings[i].splitlines()
+        page_num += 1
+        
+        # iterate through each line in the page
+        for line in text_on_page:
+            # if line starts with next problem, set curr_problem to be next_problem, and set next_problem to be the next problem in the queue
+            if line.startswith(next_problem):
+                curr_problem = next_problem
+                if problem_queue:
+                    next_problem = problem_queue.pop(0)
+                else: 
+                    next_problem = None
+
+            # add each new problem to the hashmap. Store the current page numbr as the first page associated with the problem
+            if curr_problem not in problem_pages_map:
+                    problem_pages_map[curr_problem] = []
+                    problem_pages_map[curr_problem].append(page_num)
+
+            # if answer to a question is continued on the next page, add the next page to the list that contains the number of pages for the current problem
+            if curr_problem in problem_pages_map and page_num not in problem_pages_map[curr_problem]:                
+                problem_pages_map[curr_problem].append(page_num)
+                        
+            # if there are no more problems in the queue, then just process the last question, then we're done
+            if not next_problem:
+                return problem_pages_map
+            
+
+# TEST USING PREDEFINED TEST CASES FOR BOTH PARAMETERS
+print(page_assigner(test1, test2))
+
+# TEST USING DOWNLOADED IMAGES FROM URLS OF AN ACTUAL PSET, AND THE LIST OF PROBLEMS STORED IN THE VARIABLE example_list_of_problems
+print(page_assigner(image_url_to_text(arr_of_img_urls), example_list_of_problems))
+
+# EVENTUAL USE CASE TO BE USED BY TAKING IN BOTH IMAGE URLS AND LIST OF PROBLEMS FROM GRADESCOPE.PY
+    
+                
+
+
+
+
